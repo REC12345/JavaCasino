9449c10056f188833d762078423cb0ac4be2d6f9
REC12345 <rileycohen79@gmail.com>
2024-10-17 15:45:38 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,7 +2,10 @@ public class Player
 {
     public static void main(String[] args) 
     {
-        
+        /* while loop to keep the casino running until the player quits.
+        * if statements to see what game the user wants to play.
+        */
+
     }
 
 }
