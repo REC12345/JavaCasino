ca1bb18031506bec47a2429489217bfafcfbe49a
REC12345 <rileycohen79@gmail.com>
2024-10-30 14:02:05 -0600

    Update DeckOfCards.java

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+
 public class DeckOfCards 
 {
-        
+    private ArrayList<Integer> cardDeck;
+    private int numCards;
+
+    public DeckOfCards(int n)
+    {
+        numCards = n;
+        cardDeck = new ArrayList<>();
+
+        for (int i = 0; i < numCards; i++)
+        {
+            cardDeck.add(i);
+        }
+    }
+
+    public int drawCard()
+    {
+        int card = 0;
+        return card;
+    }
+
+
 }
